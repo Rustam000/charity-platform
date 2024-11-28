1f98be6dc7f1d51298a5f7485ba9b73182dac098
rustamBoss11 <rus112004rus@gmail.com>
2024-11-28 15:53:58 +0600

    start project

---
+++ frontend/src/components/ProjectList.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ProjectList = () => {
+  return (
+    <div>
+      <h1>Projects</h1>
+      {/* Здесь будут отображаться проекты */}
+    </div>
+  );
+};
+
+export default ProjectList;
